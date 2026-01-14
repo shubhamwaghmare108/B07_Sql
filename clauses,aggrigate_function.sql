3a42808de226fa219b417fe28ad196d0d7e9cd7a
Shubham Waghmare <scwagh123@gmail.com>
2026-01-14 11:08:20 +0530

    Revised clauses file

--- clauses,aggrigate_function.sql
+++ clauses,aggrigate_function.sql
@@ -3,7 +3,7 @@ show tables;
 -- * is shortcut for all columns
 select * from facility;
 
--- perticular column selection
+-- particular column selection
 select facilityid,country,city from facility;
 
 -- 1+2=3
@@ -68,3 +68,60 @@ order by count(*);
 select country,facilitytype,count(*) as records
 from facility  group by country,facilitytype 
 order by count(*) desc,facilitytype desc;
+
+select * from facility;
+-- limit records for result set
+select * from facility limit 5;
+
+-- offset changes reference point
+select * from facility limit 5 offset 5;-- pagination
+select * from facility limit 5 offset 10;
+
+-- distinct use for displaying each value once only.
+select distinct(facilitytype) from facility;
+select distinct(country) from facility;
+select count(distinct(country)) from facility;
+select distinct country,city from facility;
+select distinct facilitytype,country from facility;
+
+select facilitytype,country,count(*) from facility
+group by facilitytype,country;
+
+-- in ,between,like
+select * from facility;
+select * from facility
+where country in ('france','india');
+
+select * from facility
+where country='france' or country='india';
+
+select * from facility
+where employee_count between 3500 and 3600;
+
+select * from facility 
+where employee_count>=3500 and employee_count<=3600;
+
+select * from facility
+where ManufacturerId between 1 and 10;-- included
+
+show tables;
+select * from production;
+
+select * from production
+where `date` between '2016-01-01' and '2016-02-01';-- included
+
+select * from facility;
+select * from facility
+where country between 'a' and 'j';-- text last is not included
+
+-- % any character any times
+select * from facility where country like 'c%';
+select * from facility where country like '%c%';
+select * from facility where country like '%a';
+
+-- _ fix character ,length
+select * from facility where country like '_____';-- five character name
+select * from facility where country like 'c____';
+select * from facility where country like 'in____%';
+
+-- wild card character %,_
